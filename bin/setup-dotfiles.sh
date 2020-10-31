834d2846de3730ba2dea742416f72f8fe2f9edb8
Tim Welch <tim@welchnet.org>
2020-10-31 12:07:37 -0400

    Create script to setup the dotfiles repo locally and use it for home directory

---
+++ bin/setup-dotfiles.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/sh
+
+git clone --bare git@github.com:tim-welch/dotfiles.git $HOME/dotfiles
+git --git-dir=$HOME/dotfiles worktree add -f $HOME/home master
+mv $HOME/home/.git $HOME
+rm -rf $HOME/home
+git --git-dir=$HOME/dotfiles --work-tree=$HOME checkout
+
