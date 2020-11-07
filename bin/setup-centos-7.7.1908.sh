e09efd01b093faa7e8c61fe39add9fa951796dd9
Tim Welch <tim@welchnet.org>
2020-11-06 20:33:14 -0500

    Build and install nvim
    Building neovim requires cmake. This commit uses the CentOS cmake
    which is 2.8.12 which happens to be the minimum required for neovim.

--- bin/setup-centos-7.7.1908.sh
+++ bin/setup-centos-7.7.1908.sh
@@ -3,16 +3,27 @@
 # Install gcc, git, etc.
 sudo yum group install -y "Development Tools"
 sudo yum install -y openssl openssl-devel libcurl-devel expat-devel asciidoc xmlto
+sudo yum install -y cmake
 
+# Create ~/src to hold git repos for builds
+mkdir -p ~/src
 
 # Build and install the latest git
-mkdir -p ~/src
 [[ -d ~/src/git ]] || git clone https://github.com/git/git.git ~/src/git
 cd ~/src/git
 git checkout maint
 git pull
 make prefix=/usr all doc # info - requires docbook2-texi
 sudo make prefix=/usr install install-doc # install-html install-info
+
+# Build and install neovim
+[[ -d ~/src/neovim ]] || git clone https://github.com/neovim/neovim.git ~/src/neovim
+cd ~/src/neovim
+git fetch
+git checkout stable
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
+
 
 
 echo "Success!"
