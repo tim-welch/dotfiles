5c9d2281ecca836a1e205a8fc3216848d255dba6
Tim Welch <tim@welchnet.org>
2020-11-14 15:16:06 -0500

    Move setup scripts to their own repo

--- bin/setup-dotfiles.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/bash -e
-
-git clone --bare git@github.com:tim-welch/dotfiles.git $HOME/dotfiles
-git --git-dir=$HOME/dotfiles worktree add -f $HOME/home master
-mv $HOME/home/.git $HOME
-rm -rf $HOME/home
-git --git-dir=$HOME/dotfiles --work-tree=$HOME checkout
-
